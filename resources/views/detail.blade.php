c00e9aba68b21878efb49b1cc99dd4e1ddba84ee
Vikulov Andrey <vikulov.prog@gmail.com>
2022-06-16 23:51:14 +0400

    first commit

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.base')
+
+@section('title', $bb->title)
+
+@section('main')
+    <h2>{{ $bb->title }}</h2>
+    <p>{{ $bb->content }}</p>
+    <p>{{ $bb->price }}</p>
+    <p>Автор: {{ $bb->user->name }}</p>
+    <p><a href="{{ route('index') }}">На перечень объявлений</a></p>
+@endsection('main')
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BbsController;
+use App\Http\Controllers\HomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [BbsController::class, 'index'])->name('index');
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home/add', [HomeController::class, 'showAddBbForm'])->name('bb.add');
+Route::post('/home', [HomeController::class, 'storeBb'])->name('bb.store');
+Route::get('/home/{bb}/edit', [HomeController::class, 'showEditBbForm'])->name('bb.edit');
+Route::patch('/home/{bb}', [HomeController::class, 'updateBb'])->name('bb.update');
+Route::get('/home/{bb}/delete', [HomeController::class, 'showDeleteBbForm'])->name('bb.delete');
+Route::delete('/home/{bb}', [HomeController::class, 'destroyBb'])->name('bb.destroy');
+
+Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
